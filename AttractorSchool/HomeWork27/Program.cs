a476013ee74b67db421314df7d8954444b0ab9f6
Yuri S <y.svechnikov@outlook.com>
2025-05-19 04:28:16 +0600

    Task1

---
+++ AttractorSchool/HomeWork27/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HomeWork27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 7, 4, 9, 2};
+            string[] words = { "world", "apple", "hello"};
+
+            var sortedNums = SortReverseArray<int>.Sort(nums);
+            var sortedWords = SortReverseArray<string>.Sort(words);
+            foreach (var x in sortedNums)
+            {
+                Console.WriteLine(x);
+            }
+            foreach (var x in sortedWords)
+            {
+                Console.WriteLine(x);
+            }
+
+            var sortedReversedNums = SortReverseArray<int>.Reverse(sortedNums);
+            var sortedReversedWords = SortReverseArray<string>.Reverse(sortedWords);
+            foreach (var x in sortedReversedNums)
+            {
+                Console.WriteLine(x);
+            }
+            foreach (var x in sortedReversedWords)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ AttractorSchool/HomeWork27/SortReverseArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork27
+{
+    internal class SortReverseArray<T>
+    {
+        public static T[] Sort(T[] list)
+        {
+            Array.Sort(list);
+            return list;
+        }
+
+        public static T[] Reverse(T[] list)
+        {
+            Array.Reverse(list);
+            return list;
+        }
+    }
+}
